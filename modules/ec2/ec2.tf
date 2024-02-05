00c43c3a53171334908d5cf57ed64f8ee1148e0c
gitzlaffc4 <gitzlaffc4@gmail.com>
2024-02-05 09:18:08 -0600

    PD-56 First Module

---
+++ modules/ec2/ec2.tf
@@ -0,0 +1,11 @@
+variable "ec2name" {
+  type = string
+}
+
+resource "aws_instance" "ec2" {
+  ami           = "ami-0866a04d72a1f5479"
+  instance_type = "t2.micro"
+  tags = {
+    Name = var.ec2name
+  }
+}
